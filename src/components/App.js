344ce7625d3b99f94f49d5d78defebd77c703406
JennyQ <jennifercquispe@gmail.com>
2020-04-19 13:22:21 -0400

    setting up axios, using unsplash API for images

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,30 @@
 import React from 'react';
+import axios from 'axios';
 import SearchBar from './SearchBar';
 
-const App = () => {
-    return (
-        <div className="ui container" style={{marginTop: '10px'}}>
-            <SearchBar />
-        </div>
-    );
+class App extends React.Component {
+    state = { images: [] };
+
+    async onSearchSubmit(term) {
+        console.log(term);
+        const response = await axios.get('https://api.unsplash.com/search/photos', {
+            params: { query: term },
+            headers: {
+                Authorization: hidden
+            }
+        });
+
+        console.log(response.data.results);
+    }
+
+    render() {
+        return (
+            <div className="ui container" style={{marginTop: '10px'}}>
+                <SearchBar onSubmit={this.onSearchSubmit} />
+            </div>
+        );
+    }
+    
 };
 
 export default App;
